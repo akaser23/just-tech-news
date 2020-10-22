8687e9669b739f270e8c61e50d5fb3733f4f16cd
Anna Kaser <kaserann96@gmail.com>
2020-10-21 18:26:19 -0600

    comments

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -25,18 +26,34 @@ Post.belongsToMany(User, {
 
 Vote.belongsTo(User, {
     foreignKey: 'user_id'
-  });
-  
-  Vote.belongsTo(Post, {
-    foreignKey: 'post_id'
-  });
-  
-  User.hasMany(Vote, {
-    foreignKey: 'user_id'
-  });
-  
-  Post.hasMany(Vote, {
-    foreignKey: 'post_id'
-  });
+});
 
-module.exports = { Post, User, Vote };
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { Post, User, Vote, Comment };
